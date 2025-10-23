5ba7a6e9c1f011a6bd02cfbc9100329621c5bea8
autofruit <thangphanvan96@gmail.com>
2025-10-23 12:52:54 +0700

    Update img_source.h

--- img_source.h
+++ img_source.h
@@ -11,6 +11,8 @@ extern const uint8_t servo_phase[];
 extern const uint8_t F2_phase[];
 extern const uint8_t F3_phase[];
 extern const uint8_t wifi[];
+extern const uint8_t face_code[];
+extern const uint8_t bank_code[];
 
 #endif
 
